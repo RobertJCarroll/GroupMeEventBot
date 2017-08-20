2fb58f319c21c06b9399f286666510e9e11368e1
RobertJCarroll <ryjaek@gmail.com>
2017-08-20 21:58:01 +0000

    Update server file and fix import logic

--- init.py
+++ init.py
@@ -1,3 +1,6 @@
-import server
+import json
+from gm_bot_listener import GMBotHandler,GMBotServer
 
-s = GMBotServer(40001,'/var/bot0msg/')
+with open('my_creds.json') as json_creds: creds = json.load(json_creds)
+
+s = GMBotServer(creds.get('port'),creds.get('msg_dump_dir'))
