53a2bc583bb1103b7254102947fe10de26deefd1
RobertJCarroll <ryjaek@gmail.com>
2017-08-20 21:53:47 +0000

    Update server file and fix import logic

---
+++ gm_bot_listener.py
@@ -0,0 +1,29 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+from datetime import datetime
+
+class GMBotHandler(BaseHTTPRequestHandler):
+    def __init__(self,msg_dump_dir,*args):
+        self.msg_dump_dir=msg_dump_dir
+        BaseHTTPRequestHandler.__init__(self,*args)
+        return
+
+    def do_POST(self):
+        if self.headers.get('User-Agent')!='GroupMeBotNotifier/1.0': return
+        self.send_response(200)
+        self.end_headers()
+        print(self.client_address)
+        message=self.rfile.read(int(self.headers.get('content-length')))
+        print(self.headers.items())
+        #Note that this will miss multiple messages per second
+        with open(self.msg_dump_dir+'msg_'+datetime.now().strftime('%Y-%m-%d_%H:%M:%S'),'wb') as out:
+            out.write(message)
+        return
+
+class GMBotServer:
+    def __init__(self,port,msg_dump_dir):
+        def handler(*args):
+            GMBotHandler(msg_dump_dir,*args)
+        server = HTTPServer(('',port),handler)
+        server.serve_forever()
+
+
